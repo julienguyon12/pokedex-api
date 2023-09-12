dfbf5392273b9ac908e2861ad126abd193527e86
julienguyon12 <julien.guyon@2018.icam.fr>
2023-09-12 12:31:50 +0200

    remove dev dependencies

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 const express = require("express");
-const morgan = require("morgan");
 const favicon = require("serve-favicon");
 const bodyParser = require("body-parser");
 const sequelize = require("./src/db/sequelize");
 const cors = require("cors");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app
   .use(favicon(__dirname + "/favicon.ico"))
-  .use(morgan("dev"))
   .use(bodyParser.json())
   .use(cors());
 
 sequelize.initDb();
+app.get("/", (req, res) => {
+  res.json("Hello render");
+});
 
 require("./src/routes/findAllPokemons")(app);
 require("./src/routes/findPokemonByPk")(app);
