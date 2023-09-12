c1c48011ad2ad611191493df146534f6258e0b53
julienguyon12 <julien.guyon@2018.icam.fr>
2023-09-12 13:06:12 +0200

    add database

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -4,20 +4,29 @@ const UserModel = require("../models/user");
 const pokemons = require("./mock-pokemon");
 const bcrypt = require("bcrypt");
 
-const sequelize = new Sequelize("pokedex", "root", "RAfikan31!!", {
-  host: "localhost",
-  dialect: "mysql",
-  //   dialectOptions: {
-  //     timezone: "Etc/GMT-2",
-  //   },
-  //   logging: false,
-});
-
+let sequelize;
+if (process.env.NODE_ENV === "production") {
+  sequelize = new Sequelize(
+    "bzlrzsnsqajaqbfrmemx",
+    "ucmyveelxa5gwhf0",
+    "FLEHQbESDo2QKV2aGmPf",
+    {
+      host: "bzlrzsnsqajaqbfrmemx-mysql.services.clever-cloud.com",
+      dialect: "mysql",
+    }
+  );
+}
+{
+  sequelize = new Sequelize("pokedex", "root", "RAfikan31!!", {
+    host: "localhost",
+    dialect: "mysql",
+  });
+}
 const Pokemon = PokemonModel(sequelize, DataTypes);
 const User = UserModel(sequelize, DataTypes);
 
 const initDb = () => {
-  return sequelize.sync({ force: true }).then((_) => {
+  return sequelize.sync().then((_) => {
     pokemons.map((pokemon) => {
       Pokemon.create({
         name: pokemon.name,
