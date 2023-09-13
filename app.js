587b493d1331ead50363f32e2f868c9469b89b4e
julienguyon12 <julien.guyon@2018.icam.fr>
2023-09-13 08:31:58 +0200

    modify env file

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const favicon = require("serve-favicon");
 const bodyParser = require("body-parser");
--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -4,29 +4,44 @@ const UserModel = require("../models/user");
 const pokemons = require("./mock-pokemon");
 const bcrypt = require("bcrypt");
 
-let sequelize;
-if (process.env.NODE_ENV === "production") {
-  sequelize = new Sequelize(
-    "bzlrzsnsqajaqbfrmemx",
-    "ucmyveelxa5gwhf0",
-    "FLEHQbESDo2QKV2aGmPf",
-    {
-      host: "bzlrzsnsqajaqbfrmemx-mysql.services.clever-cloud.com",
-      dialect: "mysql",
-    }
-  );
-}
-{
-  sequelize = new Sequelize("pokedex", "root", "RAfikan31!!", {
-    host: "localhost",
+// const sequelize = new Sequelize({
+//   username: process.env.DATABASE_USERNAME,
+//   password: process.env.DATABASE_PASSWORD,
+//   database: process.env.DATABASE_NAME,
+//   dialect: "mysql",
+//   port: process.env.DATABASE_PORT,
+//   host: process.env.DATABASE_HOST,
+// });
+
+// let sequelize;
+// if (process.env.NODE_ENV === "production") {
+// const sequelize = new Sequelize(
+//   "bzlrzsnsqajaqbfrmemx",
+//   "ucmyveelxa5gwhf0",
+//   "FLEHQbESDo2QKV2aGmPf",
+//   {
+//     host: "bzlrzsnsqajaqbfrmemx-mysql.services.clever-cloud.com",
+//     dialect: "mysql",
+//   }
+// );
+// }
+// {
+const sequelize = new Sequelize(
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_USERNAME,
+  process.env.DATABASE_PASSWORD,
+  {
+    host: process.env.DATABASE_HOST,
     dialect: "mysql",
-  });
-}
+  }
+);
+console.log(process.env.DATABASE_HOST);
+
 const Pokemon = PokemonModel(sequelize, DataTypes);
 const User = UserModel(sequelize, DataTypes);
 
 const initDb = () => {
-  return sequelize.sync().then((_) => {
+  return sequelize.sync({ force: true }).then((_) => {
     pokemons.map((pokemon) => {
       Pokemon.create({
         name: pokemon.name,
